d952790de6f03f0f5c17f0cb676eb31588ead33e
Ryan Bartruff <rbartruff@gmail.com>
2023-03-02 11:03:34 -0800

    graphql crud

--- dist/index.js
+++ dist/index.js
@@ -4,11 +4,13 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const core_1 = require("@mikro-orm/core");
+require("reflect-metadata");
 const mikro_orm_config_1 = __importDefault(require("./mikro-orm.config"));
 const express_1 = __importDefault(require("express"));
 const apollo_server_express_1 = require("apollo-server-express");
 const type_graphql_1 = require("type-graphql");
 const hello_1 = require("./resolvers/hello");
+const post_1 = require("./resolvers/post");
 const main = async () => {
     const orm = await core_1.MikroORM.init(mikro_orm_config_1.default);
     await orm.getMigrator().up();
@@ -19,9 +21,10 @@ const main = async () => {
     });
     const apolloSever = new apollo_server_express_1.ApolloServer({
         schema: await (0, type_graphql_1.buildSchema)({
-            resolvers: [hello_1.HelloResolver],
+            resolvers: [hello_1.HelloResolver, post_1.PostResolver],
             validate: false
         }),
+        context: () => ({ em: emFork })
     });
     await apolloSever.start();
     apolloSever.applyMiddleware({ app });
